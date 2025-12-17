865eecce7d1fc09d9873781f84dfd52fc733c958
Aayush5tha <139870614+Aayush5tha@users.noreply.github.com>
2025-12-17 12:09:57 +0545

    Update app.py

--- .github/workflows/app.py
+++ .github/workflows/app.py
@@ -4,11 +4,5 @@ def add(a, b):
 #print name
 def greet(name):
     return f"Hello, {name}!"
-# test_app.py
-from app import add, greet
-def test_add():
-    assert add(2, 3) == 5
-​
-def test_greet():
-    assert greet("Alice") == "Hello, Alice!"
+
 
